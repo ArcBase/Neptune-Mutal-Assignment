43385c631ddddcc73c39f680d6ab9d39a8e31c79
ArcBase <taiwoadebayo4040@gmail.com>
2021-12-10 20:02:19 +0000

    added modal for connecting wallet

--- components/converterComponents/connectWalletModal.tsx
+++ components/converterComponents/connectWalletModal.tsx
@@ -1,8 +1,19 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Web3 from "web3";
+import { connect } from "react-redux";
+import { processWalletState } from '../../redux/actions/connectWeb'
+
+declare global {
+    interface Window {
+        web3: any;
+        ethereum?: any;
+    }
+}
+
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -16,10 +27,26 @@ const style = {
     p: 4,
 };
 
-const ConnectWalletModal = () => {
+const ConnectWalletModal = (props: any) => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+
+    const connectWallet = async (e:any) => {
+        e.preventDefault()
+        if (typeof window !== undefined) {
+            if (window.web3) {
+                window.web3 = new Web3(window.ethereum);
+
+                await window.ethereum.send("eth_requestAccounts");
+                const web3 = window.web3
+                const accounts = await web3.eth.getAccounts();
+                props.walletConnect(accounts[0])
+
+            }
+        }
+    };
+
 
     return (
         <div>
@@ -31,24 +58,24 @@ const ConnectWalletModal = () => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
+
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                         <div className="account-data-section">
                             <div className="account-data-column">
                                 <div className="account-data-rows">
                                     <div className="account-data-row-left">
-                                    <button
-                                className="mint-button">
-                               Disconnect
-                            </button>
+                                        <button
+                                        onClick={(e)=>{connectWallet(e)}}
+                                            className="mint-button">
+                                            Connect Wallet
+                                        </button>
                                     </div>
                                     <div className="account-data-row-right">
-                                    <button
-                                className="cancel-button">
-                               Disconnect
-                            </button>
+                                        <button
+                                            onClick={handleClose}
+                                            className="cancel-button">
+                                            Cancel
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -62,4 +89,17 @@ const ConnectWalletModal = () => {
     );
 }
 
-export default ConnectWalletModal
+const mapStateToProps = (state: any) => {
+    return {
+        walletAddress: state.walletConnect.walletAddress
+    };
+};
+
+
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        walletConnect: (thePublicAddress: string) => dispatch(processWalletState(thePublicAddress)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConnectWalletModal);
